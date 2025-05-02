88ab42fee39d60ef48a8922df075d98488cb8ff4
MichaelAIM <michael.aguirre.saavedra@gmail.com>
2025-05-02 12:03:50 -0400

    Marcas Listas

--- src/services/marcaService.ts
+++ src/services/marcaService.ts
@@ -42,7 +42,7 @@ const getAllMarcas = async () => {
       console.error("Error desconocido:", error);
     }
   }
-}
+};
 
 // Método POST: Para agregar una nueva categoria
 const agregarMarca = async (marca: { nombre: string }) => {
@@ -63,8 +63,58 @@ const agregarMarca = async (marca: { nombre: string }) => {
     }
   }
 };
+
+// Método PUT: Para actualizar una Marca existente
+const actualizarMarca = async (
+  id: number,
+  marcaActualizada: { nombre: string }
+) => {
+  const loginStore = useLoginStore();
+  const token = String(loginStore.token); // Obtén el token actualizado
+  try {
+    const response = await axios.put(
+      `${API_URL}/marca/${id}`,
+      marcaActualizada,
+      {
+        headers: {
+          "x-token": token,
+        },
+      }
+    );
+    return response.data; // Devuelve la respuesta de la API
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+    } else {
+      console.error("Error desconocido:", error);
+    }
+  }
+};
+
+// Método DELETE: Para eliminar una Marca
+const eliminarMarca = async (id: number) => {
+  const loginStore = useLoginStore();
+  const token = String(loginStore.token); // Obtén el token actualizado
+  try {
+    const response = await axios.delete(`${API_URL}/marca/${id}`, {
+      headers: {
+        "x-token": token,
+      },
+    });
+    return response.data; // Devuelve la respuesta de la API
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+    } else {
+      console.error("Error desconocido:", error);
+    }
+  }
+};
+
 export default {
   getMarcas,
   agregarMarca,
-  getAllMarcas
+  getAllMarcas,
+  actualizarMarca,
+  eliminarMarca,
 };
